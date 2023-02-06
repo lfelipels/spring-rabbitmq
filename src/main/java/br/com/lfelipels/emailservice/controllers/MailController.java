d6e6aef7e14336e984ed99186b7d536a4e9563e3
lfelipels <felipe.lima@sti.ufc.br>
2023-02-06 15:59:45 -0300

    corrige controller de envio de email

--- src/main/java/br/com/lfelipels/emailservice/controllers/MailController.java
+++ src/main/java/br/com/lfelipels/emailservice/controllers/MailController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lfelipels.emailservice.dtos.MailMessageDto;
-import br.com.lfelipels.emailservice.services.SendMail;
 import contracts.SendMailContract;
 
 @RestController
@@ -22,14 +21,10 @@ public class MailController {
     @ResponseBody
     @PostMapping(value = "/simple", produces = MediaType.APPLICATION_JSON_VALUE)
     public void send(
-            @RequestBody MailMessageDto messageDto) throws Exception {
+        @RequestBody MailMessageDto messageDto
+    ) throws Exception {
         try {
-
-            for (int i = 0; i < 50; i++) {
-                messageDto.setSubject("Mail " + i + 1);
-                this.sendMail.send(messageDto);
-            }
-
+            this.sendMail.send(messageDto);
         } catch (Exception e) {
             throw new Exception("Não foi possível enviar o email");
         }
